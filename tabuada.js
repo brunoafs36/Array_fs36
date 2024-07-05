1237d954d69d5266e4d7c8363d32c219d33d6c14
BRUNO <BRUNO6053@DIGITALCOLLEGE.LOCAL>
2024-07-04 21:31:01 -0300

    Adicionando funções

--- tabuada.js
+++ tabuada.js
@@ -1,8 +1,34 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-numeros.forEach(function(v, i) {
-    const n = 1;
-    console.log(n +"x" + v + "=" +(n*v));
+// numeros.forEach(function(v, i) {
+//     const n = 1;
+//     console.log(n +"x" + v + "=" +(n*v));
 
-})
+// })
+
+const tabuadaResultado = document.getElementById("tabuada");
+
+function gerarTabuada() {
+    const numero = parseInt(document.getElementById("numero").value);
+    const de = parseInt(document.getElementById("de").value);
+    const ate = parseInt(document.getElementById("ate").value);
+
+//    console.log(numero,de,ate);
+
+let tabuada = '';
+
+   Array.from({length: ate }, function(v, k) {
+    const multiplicador = k+1;
+    const linhaTabuada = numero + ' x ' + multiplicador + ' = ' + (numero * multiplicador) + '\n';
+    console.log(linhaTabuada);
+
+    tabuada = tabuada + linhaTabuada;
+
+   })
+
+   console.log(tabuada);
+
+   tabuadaResultado.innerText = tabuada;
+
+};
 
